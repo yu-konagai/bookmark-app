d5ba2588c84dbdac273464be0426121b8da5fa6a
yu-konagai <yu.kii708117@gmail.com>
2026-09-03 08:32:20 +0900

    ブックマークの追加機能と一覧ロジックを追加

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+
+const form=document.getElementById("bookmark-form") as HTMLFormElement;
+const titleInput=document.getElementById("title") as HTMLInputElement;
+const urlInput = document.getElementById("url") as HTMLInputElement;
+const memoTextarea = document.getElementById("memo") as HTMLTextAreaElement;
+
+const bookmarkList = document.getElementById("bookmark-list") as HTMLDivElement;
+type Bookmark={
+    id:number;
+    title:string;
+    url:string;
+    memo:string;
+    isFavorite:boolean;
+}
+
+const bookmarks:Bookmark[]=[];
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();//ページリロードを止める。リロードすると入力した値が消えるから。
+    const title = titleInput.value;
+    const url   = urlInput.value;
+    const memo  = memoTextarea.value;
+
+    console.log(title);
+    console.log(url);
+    console.log(memo);
+
+const newBookmark: Bookmark = {
+    id:Date.now(),
+    title:title,
+    url:url,
+    memo:memo,
+    isFavorite:false,
+};//bookmarkのオブジェクトを作る
+bookmarks.push(newBookmark)//配列bookmarksに追加する
+
+renderBookmarks();//画面に表示する。
+
+})
+
+function renderBookmarks(){
+    bookmarkList.innerHTML="";//上書き防止のため一度すべて消す
+
+    bookmarks.forEach((bookmark)=>{
+        const div =document.createElement("div")//ックマークを表示するための箱（div）を作る。
+        div.innerHTML = `
+          <h3>${bookmark.title}</h3>
+          <a href="${bookmark.url}"target="_blank">${bookmarkList.url}</a>
+          <p>${bookmark.memo}</p>
+        `;
+        bookmarkList.appendChild(div);
+    
+    });
+}
